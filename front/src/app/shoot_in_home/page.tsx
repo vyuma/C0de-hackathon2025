eda97110cc84c2eb32261175c484e7c94cd960df
kawahara <lost.of.milk1@gmail.com>
2025-11-22 01:07:57 +0000

    gapが多すぎたので修正

--- front/src/app/shoot_in_home/page.tsx
+++ front/src/app/shoot_in_home/page.tsx
@@ -8,7 +8,7 @@ export default function ShootInHome() {
     const router = useRouter();
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col gap-10 py-20 px-8 text-black dark:text-zinc-50 sm:px-16">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col gap-5 py-20 px-8 text-black dark:text-zinc-50 sm:px-16">
             <h1 className="text-center text-lg font-bold">バーコードを読み取らせてください。</h1>
             <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end space-y-3">
                 <button onClick={() => {
